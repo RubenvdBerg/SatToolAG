8650bcd18c4e29e35ee791ce877ad7e5fe1faa04
RubenvdBerg <r.vandenberg0205@gmail.com>
2019-04-05 18:18:09 +0200

    End Week Update: Week 3

--- ComponentExample.py
+++ ComponentExample.py
@@ -14,4 +14,24 @@ class XComp(ExplicitComponent):
 
     def compute(self, inputs, outputs):
 
-        output[''] =
+        outputs[''] = 1
+
+
+from openmdao.api import ImplicitComponent
+
+class XComp(ImplicitComponent):
+    """"Does X"""
+
+    def setup(self):
+        #input
+        self.add_input()
+        #independent variables
+        self.add_input()
+        #output
+        self.add_output()
+
+        self.declare_partials('*','*', method='fd')
+
+    def apply_nonlinear(self, inputs, outputs, residuals):
+
+        residuals[''] = 1
